2b864c24216ba19f911a480d44c032d5553d52bf
Deloyard <denataarif@gmail.com>
2022-05-26 22:13:24 +0700

    styling dikit

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,24 @@ const Home = () => {
   return (
     <div className='bg-[#F2F2FA]'>
       <h1 className='text-3xl font-bold'>My Kanban Board</h1>
-      <List />
-      <List />
-      <List />
-      <List />
+      <div className='flex '>
+        <div className='flex-initial px-2'>
+          <List />
+        </div>
+        <div className='flex-initial px-2'>
+          <List />
+        </div>
+        <div className='flex-initial px-2'>
+          <List />
+        </div>
+        <div className='flex-initial px-2'>
+          <List />
+        </div>
+      </div>
+        
+        {/* <List />
+        <List />
+        <List /> */}
     </div>
   )
 }
